db58a2ddf4eac6d430e8c6d68d916207bdc299bb
Maksym Galchuk <maksymgalch@gmail.com>
2023-04-04 20:44:06 +0300

    Init

---
+++ Program/Program.cs
@@ -0,0 +1,68 @@
+﻿using Мatrix;
+
+
+
+while (true) {
+    Console.Write("\nEnter the dimensions of the matrix 'rows cols', or 'stop' to exit: ");
+    string? input = Console.ReadLine();
+    
+    if (input == "stop") {break;}
+    if (input == null || input.Split(' ').Length != 2) {
+        Console.WriteLine("Invalid format.");
+        continue;
+    }
+    
+    int nRows = int.Parse(input.Split(' ')[0]);
+    int nCols = int.Parse(input.Split(' ')[1]);
+    Matrix m = new Matrix(nRows, nCols);
+
+    while (true) {
+        Console.WriteLine("Enter 'w' to write or 'r' to read, or any other key to exit.");
+        char c = Console.ReadKey().KeyChar;
+        if (c == 'w') {
+            Console.WriteLine("\nEnter the elements of the matrix in format 'row col value', or 'stop' to stop.");
+            while (true) {
+                input = Console.ReadLine();
+                if (input == "stop") {break;}
+                if (input == null || input.Split(' ').Length != 3) {
+                    Console.WriteLine("Invalid format.");
+                    continue;
+                }
+                
+                try {
+                    int row = int.Parse(input.Split(' ')[0]);
+                    int col = int.Parse(input.Split(' ')[1]);
+                    int value = int.Parse(input.Split(' ')[2]);
+                    m[row, col] = value;
+                } catch (IndexOutOfRangeException) {
+                    Console.WriteLine("Index out of range.");
+                } catch (FormatException) {
+                    Console.WriteLine("Invalid format.");
+                }
+            }
+        } else if (c == 'r') {
+            Console.WriteLine("\nEnter the elements of the matrix in format 'row col', or 'stop' to stop.");
+            while (true) {
+                input = Console.ReadLine();
+                if (input == "stop") {break;}
+                if (input == null || input.Split(' ').Length != 2) {
+                    Console.WriteLine("Invalid format.");
+                    continue;
+                }
+                
+                try {
+                    int row = int.Parse(input.Split(' ')[0]);
+                    int col = int.Parse(input.Split(' ')[1]);
+                    Console.WriteLine(m[row, col]);
+                } catch (IndexOutOfRangeException) {
+                    Console.WriteLine("Index out of range.");
+                } catch (FormatException) {
+                    Console.WriteLine("Invalid format.");
+                }
+            }
+        } else {
+            break;
+        }
+    }
+
+}
---
+++ Program/matrix.cs
@@ -0,0 +1,30 @@
+﻿namespace Мatrix; 
+
+public class Matrix {
+    private int[,] data;
+    
+    public Matrix(int rows, int cols) {
+        if (rows <= 0 || cols <= 0) {
+            throw new ArgumentException("Rows and cols must be positive.");
+        }
+        data = new int[rows, cols];
+    }
+
+    public int Rows {get => data.GetLength(0);}
+    public int Cols {get => data.GetLength(1);}
+    
+    public int this[int row, int col] {
+        get {
+            if (row < 0 || row >= Rows || col < 0 || col >= Cols) {
+                throw new IndexOutOfRangeException("Index out of range.");
+            }
+            return data[row, col];
+        }
+        set {
+            if (row < 0 || row >= Rows || col < 0 || col >= Cols) {
+                throw new IndexOutOfRangeException("Index out of range.");
+            }
+            data[row, col] = value;
+        }
+    }
+}
